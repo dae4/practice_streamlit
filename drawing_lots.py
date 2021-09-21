09f06d3869d718aea7614a284965ec6eb4c6c974
dae4 <3210m0123@gmail.com>
2021-09-21 15:49:51 +0900

    fix the bug

--- drawing_lots.py
+++ drawing_lots.py
@@ -5,44 +5,55 @@ import random
 
 st.title("랜덤점심")
 
-members= [str(x) for x in range(0,5,1)]
+members= [str(x) for x in range(0,30)]
 
-empty = st.empty()
 target = {}
 st.sidebar.title("휴가자는 체크해제")
-col1, col2 = st.beta_columns(2)
 
-for meber in members:
-    target[meber]=st.sidebar.checkbox(meber,True)
-temp=[]
-for i in target:
-    if target[i]==False:
-        temp.append(target[i])
+for member in members:
+    target[member]=st.sidebar.checkbox(member,True)
 
-mebers = temp
+for k,value in list(target.items()):
+    if value==False:
+        del target[k]
+
+members = list(target.keys())
+
 try :
     person=int(st.text_input("배정인원을 넣어주세요"))
+    groups=len(members)//person ## 총 생성된 조
+    even = len(members)%person  ## 마지막조 인원
 
-    groups=len(members)//person
-    even = len(members)%person
-    if even==0:
-        st.write("총 {}조".format(groups))
-        for i in range(groups):
-            st.write('{}조'.format(i+1))
-            group=random.sample(members,person)
-            for j in group:
-                members.remove(j)
-            st.write(group)
+    col1,col2 = st.columns(2)
 
-    else:
-        st.write("총 {}조".format(groups+1))
-        for i in range(groups):
-            st.write('{}조'.format(i+1))
-            group=random.sample(members,person)
-            for j in group:
-                members.remove(j)
-            st.write(group)
-        st.write('{}조'.format(groups+1))    
-        st.write(members)
+    for i in range(groups):
+
+        if i==groups-1 and len(members) > person-1 and even!=0:
+            if person!=1:
+                person-=1
+
+        group=random.sample(members,person)
+        
+        for j in group:
+            members.remove(j)
+        
+
+        group = " ".join(group)
+
+        if i%2==0:
+            with col1:
+                st.header('{}조'.format(i+1))
+                st.write('{}'.format(group))
+        else:
+            with col2:
+                st.header('{}조'.format(i+1))
+                st.write('{}'.format(group))
+          
+    if even!=0:
+        with col2:
+            members = " ".join(members)
+            st.header('{}조'.format(groups+1))
+            st.write('{}'.format(members))
+
 except:
     None
