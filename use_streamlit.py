01d6b2d335f6935714326a515f18dfa3fcd93781
dae4 <3210m0123@gmail.com>
2021-09-24 20:24:58 +0900

    select source

--- use_streamlit.py
+++ use_streamlit.py
@@ -5,7 +5,7 @@ import mediapipe as mp
 from io import BytesIO
 import numpy as np
 
-st.title("hands")
+st.title("Utils")
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -25,38 +25,48 @@ hands = mp_hands.Hands(
 # # For webcam input:
 # hands = mp_hands.Hands(
 #     min_detection_confidence=0.5, min_tracking_confidence=0.5)
-option = st.sidebar.selectbox('Please select Mode!',
+
+
+
+option = st.sidebar.selectbox('Please select mode!',
                        ['hands', 'face', 'pose'])
-	
+
+st.sidebar.write("Please select source")
+left_column, right_column = st.sidebar.columns(2)
+image = left_column.button('image')
+cam = right_column.button('webcam')
+
 st.write('You selected:', option)
 uploaded_file = st.file_uploader("Choose a Image")
+if image:
+    try:
+        bytes_data = uploaded_file.getvalue()
+        encoded_img = np.frombuffer(bytes_data, dtype = np.uint8)
+        image = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
 
-try:
-    bytes_data = uploaded_file.getvalue()
-    encoded_img = np.frombuffer(bytes_data, dtype = np.uint8)
-    image = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if option=='hands':
+            results = hands.process(image)
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    drawing_spec,
+                    drawing_spec)
+                    
+        elif option=='face':
+            results = face_mesh.process(image)
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                image=image,
+                landmark_list=face_landmarks,
+                connections=mp_face_mesh.FACEMESH_TESSELATION,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=drawing_spec)
+        st.image(image,use_column_width=True)
+    except:
+        st.write("Not Found")
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    if option=='hands':
-        results = hands.process(image)
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                drawing_spec,
-                drawing_spec)
-                
-    elif option=='face':
-        results = face_mesh.process(image)
-        for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-            image=image,
-            landmark_list=face_landmarks,
-            connections=mp_face_mesh.FACEMESH_TESSELATION,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=drawing_spec)
-    st.image(image,use_column_width=True)
-except:
-    st.write("Not Found")
-
+else:
+    st.subheader("please push button")
