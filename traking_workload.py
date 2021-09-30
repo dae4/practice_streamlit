d33a595ad309e67f6539b75a869efd6e5aca7d43
dae4 <3210m0123@gmail.com>
2021-09-30 23:52:28 +0900

    add csv file

--- traking_workload.py
+++ traking_workload.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import pandas as pd
+import os
 
 worker = ['user1']
 
-workload = pd.DataFrame(columns=worker,data=None)
+if os.path.isfile('test_csv_file.csv'):
+    workload = pd.read_csv('test_csv_file.csv')
+else:
+    workload = pd.DataFrame(columns=worker,data=None)
+
 user = st.selectbox("worker",worker)
 daily_work=st.number_input('Enter a number')
 complete = st.button("Enter")
 
 
-table = st.table(workload)
-
 if complete:
-    temp = pd.DataFrame(columns=[user],data=[daily_work])
-    table.add_rows(temp)
-    
+    insert_data = {user:[daily_work]}
+    df_to_insert = pd.DataFrame(data=insert_data)
+    workload = workload.append(df_to_insert, ignore_index=True)
+    table = st.table(workload)
+    workload.to_csv('test_csv_file.csv')
